15e34183b7d6d0f526237d8105e905ef67abf494
4l3x777 <github.com/4l3x777>
2023-12-14 02:05:26 +0300

    add protected mode descriptor builder.

--- README.md
+++ README.md
@@ -13,30 +13,34 @@
 + ```generate_random``` для создания рандомного дескриптора
 + ```make``` для создания дескриптора по заднным параметрам
 + ```info``` для печати информации о дескрипторе
++ ```make_descriptor_protected_mode``` для создания дескриптора для работы в protected mode
 
 ## Пример результата
 
 ```PYTHON
 === Descriptor===
-Descriptor bin: 1101100110001010000111111101101110010100010010101011110100100101
-Descriptor hex: d98a1fdb944abd25
+Descriptor bin (big-endian): 0000000001000000100111110000000001111110000000000000001000000000
+Descriptor hex (big-endian): 00409f007e000200
+Descriptor bin (little-endian): 0000000001000000000000000111111000000000111110010000001000000000
+Descriptor hex (little-endian): 0002007e009f4000
+Descriptor FASM assembler (little-endian): "db 0x00, 0x02, 0x00, 0x7e, 0x00, 0x9f, 0x40, 0x00"
 === Base address ===
-Base address bin: 11011001110110111001010001001010
-Base address hex: d9db944a
+Base address bin (big-endian): 00000000000000000111111000000000
+Base address hex (big-endian): 00007e00
 === Segment limit ===
-Segment limit bin: 10101011110100100101
-Segment limit hex: abd25f
+Segment limit bin (big-endian): 000000000000001000000000
+Segment limit hex (big-endian): 000200
 === Access bits ===
 ╒═════╤═════╤═════╤═══════╕
 │   G │   D │   L │   AVL │
 ╞═════╪═════╪═════╪═══════╡
-│   1 │   0 │   0 │     0 │
+│   0 │   1 │   0 │     0 │
 ╘═════╧═════╧═════╧═══════╛
 === Flags bits ===
 ╒═════╤═══════╤═════╤══════════╤═════════════════════╤═══════════════════════╤══════════════╕
 │   P │   DPL │   S │   Access │   Accessibility(RX) │   Subtype(Conforming) │   Type(Code) │
 ╞═════╪═══════╪═════╪══════════╪═════════════════════╪═══════════════════════╪══════════════╡
-│   0 │     0 │   1 │        1 │                   1 │                     1 │            1 │
+│   1 │     0 │   1 │        1 │                   1 │                     1 │            1 │
 │     │     0 │     │          │                     │                       │              │
 ╘═════╧═══════╧═════╧══════════╧═════════════════════╧═══════════════════════╧══════════════╛
 ```
@@ -46,3 +50,4 @@ Segment limit hex: abd25f
 + <https://www.codeproject.com/Articles/45788/The-Real-Protected-Long-mode-assembly-tutorial-for>
 + <https://wasm.in/blogs/category/zaschischennyj-rezhim.20/?page=2>
 + <http://sasm.narod.ru/docs/pm/pm_main.htm>
++ <https://www.cs.bham.ac.uk/~exr/lectures/opsys/10_11/lectures/os-dev.pdf>
--- descriptor_builder.py
+++ descriptor_builder.py
@@ -1,6 +1,8 @@
 import random
 from bitarray import bitarray
 from tabulate import tabulate
+from textwrap import wrap
+import struct
 
 class DescriptorBuilder:
 
@@ -12,6 +14,103 @@ class DescriptorBuilder:
                 random.choice([True, False]),
                 random.choice([True, False]))
 
+    # from https://www.cs.bham.ac.uk/~exr/lectures/opsys/10_11/lectures/os-dev.pdf
+    def make_descriptor_protected_mode(self, base_address: bitarray, limit: bitarray, readable: bool, executable: bool, writable: bool, limit_exented: bool = True) -> bitarray:
+        # big endian concept
+        descriptor = bitarray()
+        
+        if len(base_address) != 32:
+            raise ValueError("base_address must be 32 bits")
+        
+        if len(limit) != 20:
+            raise ValueError("limit must be 20 bits")
+        
+        # parsing base
+        base15_0 = base_address[16:32]
+        base23_16 = base_address[8:16]
+        base31_24 = base_address[0:8]
+        
+        # parsing limit
+        seg_length_15_0 = limit[4:20]
+        seg_length_19_16 = limit[0:4]
+        
+        # add base31_24
+        descriptor.extend(base31_24)
+        
+        # create access
+        AVL = bitarray('0')
+        L = bitarray('0')
+        D = bitarray('1')
+
+        # A 20-bit value describing the length of the segment. If the G flag (see below) is not set, this value represents the actual segment length. If the G flag is set, this value is multiplied with 4096 to represent the segment length. So if you set it to FFFFFh (20 bits) and G is set, it is 10000h * 4096 = 4GB.
+        if limit_exented:
+            G = bitarray('1')
+        else:
+            G = bitarray('0')
+
+        access = bitarray()
+        access.extend(G)
+        access.extend(D)
+        access.extend(L)
+        access.extend(AVL)
+        
+        # add access
+        descriptor.extend(access)
+
+        # add seg_length_19_16
+        descriptor.extend(seg_length_19_16)
+
+        # create flags
+        if ((not readable and executable) or (readable and executable)) and not writable:
+            Type = bitarray('1')
+            Subtype = bitarray('1')
+            Access = bitarray('1')
+            if readable:
+                Accessibility = bitarray('1')
+            else:
+                Accessibility = bitarray('0')
+        elif executable and writable:
+            print('Note that a code segment is not writable.\nHowever, because segment base addresses can overlap, you can create a writable data segment with the same base address and limit of a code segment.')
+            return None
+        elif not readable and not executable and not writable:
+            print('Error: Memory without attributes')
+            return None
+        else:  
+            Type = bitarray('0')
+            Subtype = bitarray('0')
+            Access = bitarray('0')
+            # Data segments are always readable
+            if writable:
+                Accessibility = bitarray('1')
+            else:
+                Accessibility = bitarray('0')
+
+        S = bitarray('1')
+        DPL = bitarray('00')
+        P = bitarray('1')
+        
+        flags = bitarray()
+        flags.extend(P)
+        flags.extend(DPL)
+        flags.extend(S)
+        flags.extend(Access)
+        flags.extend(Accessibility)
+        flags.extend(Subtype)
+        flags.extend(Type)
+
+        # add flags
+        descriptor.extend(flags)
+
+        # add base23_16
+        descriptor.extend(base23_16)
+
+        # add base15_0
+        descriptor.extend(base15_0)
+
+        # add seg_length_15_0
+        descriptor.extend(seg_length_15_0)
+        
+        return descriptor
 
     def make(self, base_address: bitarray, limit: bitarray, readable: bool, executable: bool, writable: bool) -> bitarray:
         # big endian concept
@@ -109,8 +208,11 @@ class DescriptorBuilder:
             return
 
         print("=== Descriptor===")
-        print("Descriptor bin:", descriptor.to01())
-        print("Descriptor hex:", bytearray(descriptor).hex())
+        print("Descriptor bin (big-endian):", descriptor.to01())
+        print("Descriptor hex (big-endian):", bytearray(descriptor).hex())
+        print("Descriptor bin (little-endian):", descriptor.to01()[::-1])
+        print("Descriptor hex (little-endian):", bytearray(descriptor)[::-1].hex())
+        print("Descriptor FASM assembler (little-endian):", '"db ' + ', '.join('0x' + i   for i in wrap(bytearray(descriptor)[::-1].hex(), width=2)) + '"')
         
         print("=== Base address ===")
         base31_24 = descriptor[0:8]
@@ -122,19 +224,20 @@ class DescriptorBuilder:
         base_address.extend(base23_16)
         base_address.extend(base15_0)
 
-        print("Base address bin:", base_address.to01())
-        print("Base address hex:", bytearray(base_address).hex())
+        print("Base address bin (big-endian):", base_address.to01())
+        print("Base address hex (big-endian):", bytearray(base_address).hex())
         
         print("=== Segment limit ===") 
         seg_length_15_0 = descriptor[48:64]
         seg_length_19_16 = descriptor[12:16]
         
         segment_limit = bitarray()
+        segment_limit.extend('0000')
         segment_limit.extend(seg_length_19_16)
         segment_limit.extend(seg_length_15_0)
         
-        print("Segment limit bin:", segment_limit.to01())
-        print("Segment limit hex:", bytearray(segment_limit).hex())
+        print("Segment limit bin (big-endian):", segment_limit.to01())
+        print("Segment limit hex (big-endian):", bytearray(segment_limit).hex())
         
         print("=== Access bits ===")    
         access = descriptor[8:12]
@@ -172,8 +275,27 @@ class DescriptorBuilder:
                 }, headers="keys", tablefmt="fancy_outline"))
 
 if __name__ == "__main__":
-    builder = DescriptorBuilder()
-    descriptor = builder.generate_random()
-    builder.info(descriptor)
+    builder = DescriptorBuilder() 
+    code_seg_descriptor = builder.make_descriptor_protected_mode(
+        base_address=bitarray(f'{0x7e00:0>32b}'),
+        limit=bitarray(f'{0x200:0>20b}', endian='big'),
+        executable=True,
+        readable=True,
+        writable=False,
+        limit_exented=False
+        )   
+    builder.info(code_seg_descriptor)
 
+    data_seg_descriptor = builder.make_descriptor_protected_mode(
+        base_address=bitarray(f'{0x8000:0>32b}'),
+        limit=bitarray(f'{0x200:0>20b}', endian='big'),
+        executable=False,
+        readable=False,
+        writable=True,
+        limit_exented=False
+        )
+    builder.info(data_seg_descriptor)   
+
+    descriptor = builder.generate_random()
+    builder.info(descriptor) 
    
